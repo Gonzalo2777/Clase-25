ef90e859c891d47e0476fc9f0b91283b12784fa9
Gonzalo2777 <93409295+Gonzalo2777@users.noreply.github.com>
2022-01-04 16:50:06 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var cosita1;
+var cosita2;
+var cosita3;
+var cosita4;
+
+var btn2;
+
+var angle=60;
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+  
+  
+   btn2 = createImg('up.png');
+  btn2.position(350,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+
+  cosita1 = new Ground( 100, 300, 100, 20);
+  cosita2 = new Ground( 200, 300, 100, 20);
+  cosita3 = new Ground( 300, 300, 100, 20);
+  cosita4 = new Ground( 400, 300, 100, 20);
+
+  
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+  
+ 
+
+  ellipse(ball.position.x,ball.position.y,20);
+ 
+ 
+//console.log(ground.position.y);
+
+  cosita1.show();
+  cosita2.show();
+  cosita3.show();
+  cosita4.show();
+  Engine.update(engine);
+  
+}
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+  
